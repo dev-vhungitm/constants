d96a58b7383d5ba62224d3949fa20b6c1c6ca8e9
dev-vhungitm <dev.vhungitm@gmail.com>
2025-02-28 08:38:01 +0700

    init

---
+++ configs.ts
@@ -0,0 +1,7 @@
+const imageValidTypes = 'image/webp,image/jpeg,image/png,image/svg+xml,image/gif,image/bmp,image/tiff';
+const imageValidTypesPattern = /^image\/(webp|jpeg|jpg|png|svg|gif|bmp|tiff)$/;
+
+export const configs = {
+	imageValidTypes,
+	imageValidTypesPattern
+};
